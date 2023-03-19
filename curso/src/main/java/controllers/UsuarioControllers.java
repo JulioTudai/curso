0ff8762710448a38c675628cc64027c506efb4ab
Julio marquinez <Juliotweter@hotmail.com>
2023-03-18 21:00:06 -0300

    se agrega el proyecto

---
+++ curso/src/main/java/controllers/UsuarioControllers.java
@@ -0,0 +1,57 @@
+package controllers;
+import models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+
+public class UsuarioControllers {
+    @RequestMapping(value= "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("12312321");
+
+        return usuario;
+    }
+
+    @RequestMapping(value= "usuario32")
+    public Usuario modificar(){
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre("lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("12312321");
+
+        return usuario;
+    }
+    @RequestMapping(value= "usuario32")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre("lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("12312321");
+
+        return usuario;
+    }
+    @RequestMapping(value= "usuario32")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre("lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("12312321");
+
+        return usuario;
+    }
+}
---
+++ curso/src/main/java/models/Usuario.java
@@ -0,0 +1,59 @@
+package models;
+
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private String telefono;
+    private Long id;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
